cb4e2e1071b4bcc89d04efa78dd8588a34d7929c
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-16 21:39:50 +0300

    add detail screen

---
+++ Delegate-Protocols/Detail/View/DetailVC.swift
@@ -0,0 +1,23 @@
+//
+//  DetailVC.swift
+//  Delegate-Protocols
+//
+//  Created by Mehmet Kerim ÖZEK on 16.12.2022.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
---
+++ Delegate-Protocols/Detail/ViewModel/DetailVM.swift
@@ -0,0 +1,10 @@
+//
+//  DetailVM.swift
+//  Delegate-Protocols
+//
+//  Created by Mehmet Kerim ÖZEK on 16.12.2022.
+//
+
+import Foundation
+
+
