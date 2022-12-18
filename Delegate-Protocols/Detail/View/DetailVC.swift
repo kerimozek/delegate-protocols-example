27f349e612f3708ee81c15ed0d4c7560ec2ed2a6
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-18 23:42:51 +0300

    refactor detail

--- Delegate-Protocols/Detail/View/DetailVC.swift
+++ Delegate-Protocols/Detail/View/DetailVC.swift
@@ -23,5 +23,6 @@ class DetailVC: UIViewController {
     private func setupUI() {
         detailLabel.text = details?.body
         titleLabel.text = details?.title
+        print("true")
     }
 }
